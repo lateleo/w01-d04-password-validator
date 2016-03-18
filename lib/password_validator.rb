63ad6956fed8c2f922585fd03472c8c54aace7f0
Art Burtch <artburtch@huskers.unl.edu>
2016-03-18 16:54:06 -0500

    replaced 'password' regex line with generalized forbidden words checker for ease of use

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -1,60 +1,20 @@
 #require "pry"
 
 def valid_password?(password)
-    at_least_one_caps?(password) &&
-    at_least_eight_characters?(password) &&
-    at_least_one_lower_case?(password) &&
-    at_least_one_number?(password) &&
-    at_least_one_non_alphanumeric?(password) &&
-    doesnt_contain_password?(password)
+  (password != password.downcase) &&
+  (password != password.upcase) &&
+  (password.length >= 8) &&
+  (/[0-9]/.match(password) != nil) &&
+  (/[^0-9A-Z]/.match(password.upcase) != nil) &&
+  not_forbidden?(password)
 end
 
-# Takes a string
-# Returns true or false depending on if the string
-# has at least one capital letter
-def at_least_one_caps?(password)
-  password != password.downcase
+def not_forbidden?(password)
+  forbidden_words = ["password"] # can add words to this list as necessary.
+  result = true
+  forbidden_words.each {|word| result = false if password.downcase.include?(word)}
+  result
 end
 
-# Takes a string
-# Returns true or false depending on if the string
-# has at least one lower-case letter
-def at_least_one_lower_case?(password)
-  password != password.upcase
-end
-
-# Takes a string
-# Returns true or false depending on if the string
-# is at least 8 characters long.
-def at_least_eight_characters?(password)
-  password.length >= 8
-end
-
-# Takes a string
-# Returns true or false depending on if the string
-# contains at least one numeric character.
-def at_least_one_number?(password)
-  password.chars.each do |char|
-    return true if ("0".."9").include?(char)
-  end
-  false
-end
-
-# Takes a string
-# Returns true or false depending on if the string
-# contains at least one non-alphanumeric character.
-def at_least_one_non_alphanumeric?(password)
-  password.upcase.chars.each do |char|
-    return true if !("0".."9").include?(char) && !("A".."Z").include?(char)
-  end
-  false
-end
-
-# Takes a string
-# Returns true or false depending on if the string
-# contains the substring "password".
-def doesnt_contain_password?(password)
-  !password.downcase.include?("password")
-end
 
 #binding.pry
--- tests/password_validator_test.rb
+++ tests/password_validator_test.rb
@@ -4,82 +4,41 @@ require_relative "../lib/password_validator.rb"
 class PasswordValidatorTest < Minitest::Test
   # We'll try a password with all-lower-case-letters
   def test_rejects_all_lower_case_passwords
-    result = at_least_one_caps?("1abjils&a")
+    result = valid_password?("1abjils&a")
     refute(result, "'1abjils&a' should be invalid because it contains no caps")
-  end
-
-  # We try a password with a valid mix of upper and lower-case characters
-  # on the method that checks for caps.
-  def test_caps_accepts_mixed_case_passwords
-    result = at_least_one_caps?("1Abjils&a")
-    assert(result, "'1Abjils&a' should be valid because it is mixed case")
   end
 
   # We'll try a password with all-caps
   def test_rejects_all_caps_passwords
-    result = at_least_one_lower_case?("1ABJILS&A")
+    result = valid_password?("1ABJILS&A")
     refute(result, "'1ABJILS&A' should be invalid because it contains no lower-case letters")
-  end
-
-  # We try a password with a valid mix of upper and lower-case characters
-  # on the method that checks for lowercase.
-  def test_lowercase_accepts_mixed_case_passwords
-    result = at_least_one_lower_case?("1Abjils&a")
-    assert(result, "'1Abjils&a' should be valid because it is mixed case")
-  end
-
-  # We try a password of 8 characters, expecting success
-  def test_accepts_password_of_8_characters
-    result = at_least_eight_characters?("1Abjils&")
-    assert(result, "'1ABjils&' has 8 characters, should be valid")
   end
 
   # We try a password of only 7 characters, expecting rejection
   def test_rejects_password_of_7_characters
-    result = at_least_eight_characters?("1Abils&")
-    refute(result, "'1ABils&' has 8 characters, should be valid")
-  end
-
-  # We try a valid password with numbers
-  # on the method that checks for numbers.
-  def test_accepts_passwords_with_numbers
-    result = at_least_one_number?("1Abjils&a")
-    assert(result, "'1Abjils&a' should be valid because it contains a number")
+    result = valid_password?("1Abils&")
+    refute(result, "'1ABils&' should be invalid because it only has 7 characters")
   end
 
   # We try a password with no numbers
   # on the method that checks for numbers.
   def test_rejects_passwords_without_numbers
-    result = at_least_one_number?("Abjils&a")
+    result = valid_password?("Abjils&a")
     refute(result, "'Abjils&a' should be invalid because it contains no numbers")
-  end
-
-  # We try a valid password with non-alphanumeric characters
-  # on the method that checks for non-alphanumeric characters.
-  def test_accepts_passwords_with_non_alphanumerics
-    result = at_least_one_non_alphanumeric?("1Abjils&a")
-    assert(result, "'1Abjils&a' should be valid because it contains a non-alphanumeric character")
   end
 
   # We try an invalid password without non-alphanumeric characters
   # on the method that checks for non-alphanumeric characters.
   def test_rejects_passwords_without_non_alphanumerics
-    result = at_least_one_non_alphanumeric?("1Abjilsa")
-    refute(result, "'1Abjils&a' should be invalid because it doesn't contain a non-alphanumeric character")
+    result = valid_password?("1Abjilsa")
+    refute(result, "'1Abjils&a' should be invalid because it has no non-alphanumeric characters")
   end
 
   # We try an invalid password containing the substring "password"
   # on the method that checks the presence of the substring "password".
-  def test_rejects_passwords_containing_password
-    result = doesnt_contain_password?("1Abjpasswordilsa")
-    refute(result, "'1Abjpasswordilsa' should be invalid because it contains 'password'")
-  end
-
-  # We try a valid password lacking the substring "password"
-  # on the method that checks the presence of the substring "password".
-  def test_accepts_passwords_not_containing_password
-    result = doesnt_contain_password?("1Abjpassilsa")
-    assert(result, "'1Abjpassilsa' should be valid because it doesn't contain 'password'")
+  def test_rejects_passwords_containing_forbidden_words
+    result = valid_password?("1Abjpasswordils&a")
+    refute(result, "'1Abjpasswordils&a' should be invalid because it contains 'password'")
   end
 
   def test_valid_password
